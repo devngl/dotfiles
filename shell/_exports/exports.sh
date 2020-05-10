a8c60dd23773625d3f0d6b7b48727119bc79dfdd
Ángel Marqués <g89.angel@gmail.com>
2020-05-10 16:39:20 +0200

    Add coreutils binaries to PATH

--- shell/_exports/exports.sh
+++ shell/_exports/exports.sh
@@ -1,1 +1,2 @@
-export PATH=$PATH:$HOME/bin:$DOTFILES_PATH/bin:$DOTLY_PATH/bin:/usr/local/bin
+export PATH=$PATH:$HOME/bin:$DOTFILES_PATH/bin:$DOTLY_PATH/bin:/usr/local/bin:
+export PATH="$PATH:/usr/local/opt/coreutils/libexec/gnubin"
